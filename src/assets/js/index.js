506f483f0313e7bb0d81ff54c8213d3c01bf8e0b
armdong <armdong@163.com>
2016-09-13 12:37:30 +0800

    首页导航及轮播图完成

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -4,8 +4,45 @@
  * Date: 2016/09/12
  */
 
-(function(){
+;(function($){
 
-	
-	
-})();
+	$(function(){
+
+		// 首页Banner轮播图模块
+		var swiperBanner = null,
+			$oBannerContainer = $('#idxSlide');
+
+		fnInitBanner();
+
+		// 首页Banner轮播图
+		function fnInitBanner() {
+			var containerWidth = $oBannerContainer.width();
+	        $oBannerContainer.find('img').css({
+	            width: containerWidth,
+	            height: containerWidth / 2
+	        });
+	        if (swiperBanner != null) {
+	            swiperBanner.destroy();
+	        }
+	        swiperBanner = null;
+	        swiperBanner = new Swiper('.swiper-container', {
+	            pagination: '.swiper-pagination',
+	            width: containerWidth,
+	            height: containerWidth / 2,
+	            loop: true,
+	            centeredSlides: true,
+	            autoplay: 5000,
+	            speed: 500,
+	            autoplayDisableOnInteraction: false,
+	            preloadImages: false,
+	            lazyLoading: true
+	        });
+		}
+
+		$(window).on('resize', function() {
+            fnInitBanner();
+        });
+
+	});
+
+})(Zepto);
