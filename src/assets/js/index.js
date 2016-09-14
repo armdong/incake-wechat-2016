6cbcd67322fa7d9fecaec7b1d57e473b20b69554
armdong <armdong@163.com>
2016-09-14 09:29:12 +0800

    精品推荐轮播模块完成

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -4,45 +4,78 @@
  * Date: 2016/09/12
  */
 
-;(function($){
+;(function($, window, document) {
 
-	$(function(){
+    $(function() {
 
-		// 首页Banner轮播图模块
-		var swiperBanner = null,
-			$oBannerContainer = $('#idxSlide');
+        var swiperBanner = null,
+            swiperRecommend = null;
 
-		fnInitBanner();
+        // 首页Banner轮播图模块
+        fnInitBanner();
 
-		// 首页Banner轮播图
-		function fnInitBanner() {
-			var containerWidth = $oBannerContainer.width();
-	        $oBannerContainer.find('img').css({
-	            width: containerWidth,
-	            height: containerWidth / 2
-	        });
-	        if (swiperBanner != null) {
-	            swiperBanner.destroy();
-	        }
-	        swiperBanner = null;
-	        swiperBanner = new Swiper('.swiper-container', {
-	            pagination: '.swiper-pagination',
-	            width: containerWidth,
-	            height: containerWidth / 2,
-	            loop: true,
-	            centeredSlides: true,
-	            autoplay: 5000,
-	            speed: 500,
-	            autoplayDisableOnInteraction: false,
-	            preloadImages: false,
-	            lazyLoading: true
-	        });
-		}
+        // 精品推荐轮播图模块
+        fnInitRecommend();
 
-		$(window).on('resize', function() {
+        $(window).on('resize', function() {
             fnInitBanner();
+            fnInitRecommend();
         });
 
-	});
 
-})(Zepto);
+        // 首页Banner轮播图
+        function fnInitBanner() {
+            var $oContainer = $('#idxSlide'),
+                containerWidth = $oContainer.width();
+
+            //console.log(containerWidth);
+
+            $oContainer.find('img').css({
+                width: containerWidth,
+                height: containerWidth / 2
+            });
+            if (swiperBanner != null) {
+                swiperBanner.destroy(true, true);
+            }
+            swiperBanner = null;
+            swiperBanner = new Swiper('#idxSlide', {
+                pagination: '.swiper-pagination',
+                width: containerWidth,
+                height: containerWidth / 2,
+                loop: true,
+                centeredSlides: true,
+                autoplay: 5000,
+                speed: 500,
+                autoplayDisableOnInteraction: false,
+                preloadImages: false,
+                lazyLoading: true
+            });
+        }
+
+        // 精品推荐轮播图
+        function fnInitRecommend() {
+
+        	var $oContainer = $('#idxSection'),
+            	containerWidth = $oContainer.find('.sec-body').width();
+
+            console.log(containerWidth);
+
+            if (swiperRecommend != null) {
+                swiperRecommend.destroy(true, true);
+            }
+
+            swiperRecommend = new Swiper('#recSlide', {
+                pagination: '.swiper-pagination',
+                loop: false,
+                centeredSlides: true,
+                speed: 500,
+                preloadImages: true,
+                lazyLoading: true,
+                updateTranslate: true
+            });
+            swiperRecommend.update(true);
+        }
+
+    });
+
+})(Zepto, window, document);
