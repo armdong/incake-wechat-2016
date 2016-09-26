0e86c72a71aca4a5f869e447bc1dd16da4069fb7
armdong <armdong@163.com>
2016-09-26 17:19:43 +0800

    新品上市单个产品完成

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -27,7 +27,21 @@
             var $oContainer = $('#idxSlide'),
                 containerWidth = $oContainer.width();
 
-            //console.log(containerWidth);
+            // 渲染swiper模板
+            var _data = {
+                list: [{
+                    link: 'javascript:;',
+                    img: 'assets/imgs/slides/01.jpg'
+                }, {
+                    link: 'javascript:;',
+                    img: 'assets/imgs/slides/02.jpg'
+                }, {
+                    link: 'javascript:;',
+                    img: 'assets/imgs/slides/03.jpg'
+                }]
+            };
+            var _html = template('tplIdxSlide', _data);
+            $oContainer.find('.swiper-wrapper').html(_html);
 
             $oContainer.find('img').css({
                 width: containerWidth,
@@ -54,10 +68,101 @@
         // 精品推荐
         function fnInitRecommend() {
 
-        	var $oContainer = $('#idxSection'),
-            	containerWidth = $oContainer.find('.sec-body').width();
+            var $oContainer = $('#idxSection'),
+                containerWidth = $oContainer.find('.sec-body').width();
 
-            //console.log(containerWidth);
+            // 渲染 精品推荐 模板
+            var _data = {
+                list: [
+                    [{
+                        link: 'javascript:;',
+                        img: 'assets/imgs/index/recommend_img_01.jpg',
+                        name: {
+                            cn: '芒果拿破仑',
+                            en: 'Fresh Mango Napoleon'
+                        },
+                        desc: '芒果酸甜多汁和酥皮酥脆清香，是芒果拿破仑让人沉沦的独特口感。',
+                        price: '189',
+                        pound: '1.5',
+                        discount: {
+                            num1: '8',
+                            num2: '.5'
+                        }
+                    }, {
+                        link: 'javascript:;',
+                        img: 'assets/imgs/index/recommend_img_02.jpg',
+                        name: {
+                            cn: '芒果拿破仑',
+                            en: 'Fresh Mango Napoleon'
+                        },
+                        desc: '芒果酸甜多汁和酥皮酥脆清香，是芒果拿破仑让人沉沦的独特口感。',
+                        price: '189',
+                        pound: '1.5',
+                        discount: {
+                            num1: '8',
+                            num2: '.5'
+                        }
+                    }], [{
+                        link: 'javascript:;',
+                        img: 'assets/imgs/index/recommend_img_01.jpg',
+                        name: {
+                            cn: '芒果拿破仑',
+                            en: 'Fresh Mango Napoleon'
+                        },
+                        desc: '芒果酸甜多汁和酥皮酥脆清香，是芒果拿破仑让人沉沦的独特口感。',
+                        price: '189',
+                        pound: '1.5',
+                        discount: {
+                            num1: '8',
+                            num2: '.5'
+                        }
+                    }, {
+                        link: 'javascript:;',
+                        img: 'assets/imgs/index/recommend_img_02.jpg',
+                        name: {
+                            cn: '芒果拿破仑',
+                            en: 'Fresh Mango Napoleon'
+                        },
+                        desc: '芒果酸甜多汁和酥皮酥脆清香，是芒果拿破仑让人沉沦的独特口感。',
+                        price: '189',
+                        pound: '1.5',
+                        discount: {
+                            num1: '8',
+                            num2: '.5'
+                        }
+                    }], [{
+                        link: 'javascript:;',
+                        img: 'assets/imgs/index/recommend_img_01.jpg',
+                        name: {
+                            cn: '芒果拿破仑',
+                            en: 'Fresh Mango Napoleon'
+                        },
+                        desc: '芒果酸甜多汁和酥皮酥脆清香，是芒果拿破仑让人沉沦的独特口感。',
+                        price: '189',
+                        pound: '1.5',
+                        discount: {
+                            num1: '8',
+                            num2: '.5'
+                        }
+                    }, {
+                        link: 'javascript:;',
+                        img: 'assets/imgs/index/recommend_img_02.jpg',
+                        name: {
+                            cn: '芒果拿破仑',
+                            en: 'Fresh Mango Napoleon'
+                        },
+                        desc: '芒果酸甜多汁和酥皮酥脆清香，是芒果拿破仑让人沉沦的独特口感。',
+                        price: '189',
+                        pound: '1.5',
+                        discount: {
+                            num1: '8',
+                            num2: '.5'
+                        }
+                    }]
+                ]
+            };
+            var _html = template('tplRecSlide', _data);
+            $oContainer.find('.swiper-wrapper').html(_html);
 
             if (swiperRecommend != null) {
                 swiperRecommend.destroy(true, true);
@@ -75,34 +180,100 @@
             swiperRecommend.update(true);
         }
 
+        // 首页分类导航
+        fnInitNavCategories();
+
         // 首页公告模块
         fnInitNotice();
 
-        // 喜欢/收藏
+        // 收藏/取消收藏
         fnFavor();
+
+        // 新品上市
+        fnInitNewOnSale();
 
     });
 
+    function fnInitNewOnSale() {
+        var $oNewOnSale = $('#newOnSale');
+
+        var _data = {
+            single: true,
+            list: [{
+                link: 'javascript:;',
+                img: 'assets/imgs/index/new_img_01.jpg',
+                name: {
+                    cn: '超级蜂巢',
+                    en: 'Super hive cake'
+                },
+                desc: '芒果的酸甜“侵略”了蛋糕的每一寸，果浆比果酱更有热带水果的醇香。',
+                price: '189',
+                pound: '1.5'
+            }]
+        };
+        var _html = template('tplNewOnSale', _data);
+        $oNewOnSale.html(_html);
+    }
+
+    function fnInitNavCategories() {
+        var $oNav = $('#idxNav');
+
+        // 渲染菜单导航栏模板
+        var _data = {
+            list: [{
+                category: 'all',
+                link: 'javascript:;',
+                title: {
+                    cn: '全部商品',
+                    en: 'All cakes'
+                }
+            }, {
+                category: 'tea',
+                link: 'javascript:;',
+                title: {
+                    cn: '下午茶',
+                    en: 'Afternoon tea'
+                }
+            }, {
+                category: 'diy',
+                link: 'javascript:;',
+                title: {
+                    cn: '蛋糕定制',
+                    en: 'DIY cake'
+                }
+            }, {
+                category: 'gaw',
+                link: 'javascript:;',
+                title: {
+                    cn: 'GAW蛋糕',
+                    en: 'Genius at work'
+                }
+            }]
+        };
+        var _html = template('tplIdxNav', _data);
+        $oNav.html(_html);
+    }
+
     function fnInitNotice() {
-    	var $oNotice = $('#idxNotice'),
-    		$oNoticeMsg = $oNotice.find('.notice-msg'),
-    		$oNoticeClose = $oNotice.find('.notice-close'),
-    		tl = new TimelineLite();
+        var $oNotice = $('#idxNotice'),
+            $oNoticeMsg = $oNotice.find('.notice-msg'),
+            $oNoticeClose = $oNotice.find('.notice-close'),
+            tl = new TimelineLite();
 
-    	// 关闭公告
-    	$oNoticeClose.on('click', function(){
-    		tl.clear();
-    		tl.to($oNotice, 0.5, {
-    			opacity: 0,
-    			ease: Linear.easeOut,
-    			useFrames: true,
-    			onComplete: function() {
-    				$oNotice.remove();
-    			}
-    		});
-    	});
+        // 关闭公告
+        $oNoticeClose.on('click', function() {
+            tl.clear();
+            tl.to($oNotice, 0.5, {
+                opacity: 0,
+                ease: Linear.easeOut,
+                useFrames: true,
+                onComplete: function() {
+                    $oNotice.remove();
+                }
+            });
+        });
 
-    	// 公告轮播
+        // 公告轮播
         var marqueeText = '非真正内容非真正内容，非真正内容非真正内容非真正内容。非真正内容非真正内容，非真正内容非真正内容非真正内容。';
         $oNoticeMsg.marquee({
             text: marqueeText,
@@ -112,16 +283,11 @@
     }
 
     function fnFavor() {
-        var $oContainer = $('#idxSection'),
-            $aFavor = $oContainer.find('.favor');
+        var $oContainer = $('#idxSection');
 
-        $.each($aFavor, function(i, ele){
-            $(ele).on('click', function(){
-                $(this).toggleClass('followed');
-            });
-        });
+        $oContainer.on('click', '.favor', function() {
+            $(this).toggleClass('followed');
+        });            
     }
 
 })(Zepto, window, document);
-
-
