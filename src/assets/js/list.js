e79264650fd9108d43bf18b1e1ad6588657c4680
armdong <armdong@163.com>
2016-10-12 16:55:31 +0800

    发现->口味布局完成

--- src/assets/js/list.js
+++ src/assets/js/list.js
@@ -8,9 +8,360 @@
 
 	$(function(){
 
+		// 发现切换
+		fnChangeCategories();
+
 		// 绑定全部蛋糕
 		fnBindAll();
+
+		// 绑定口味模块
+		fnBindTaste();
+
+		// 口味切换
+		fnChangeTaste();
 	});
+
+	function fnChangeTaste() {
+		var $oSecTaste = $('#secTaste');
+
+		$oSecTaste.on('click', '.nav-item', function(){
+			$(this).addClass('active').siblings().removeClass('active');
+
+			
+		});
+	}
+
+	function fnBindTaste() {
+		var $oSecTaste = $('#secTaste'),
+			$oTasteList = $oSecTaste.find('.taste-list'),
+			$aTasteItem = $oTasteList.find('.taste-item');
+
+		var _data = {
+			cheese: {
+				list: [{
+		            link: 'javascript:;',
+		            img: 'assets/imgs/list/cake.jpg',
+		            name: {
+		                cn: '芒果拿破仑',
+		                en: 'Fresh Mango Napoleon'
+		            },
+		            price: '189',
+		            pound: '1.5'
+		        }, {
+		            link: 'javascript:;',
+		            img: 'assets/imgs/list/cake.jpg',
+		            name: {
+		                cn: '芒果拿破仑',
+		                en: 'Fresh Mango Napoleon'
+		            },
+		            price: '189',
+		            pound: '1.5'
+		        }, {
+		            link: 'javascript:;',
+		            img: 'assets/imgs/list/cake.jpg',
+		            name: {
+		                cn: '芒果拿破仑',
+		                en: 'Fresh Mango Napoleon'
+		            },
+		            price: '189',
+		            pound: '1.5'
+		        }, {
+		            link: 'javascript:;',
+		            img: 'assets/imgs/list/cake.jpg',
+		            name: {
+		                cn: '芒果拿破仑',
+		                en: 'Fresh Mango Napoleon'
+		            },
+		            price: '189',
+		            pound: '1.5'
+		        }]
+			},
+			chocolate: {
+				list: [{
+		            link: 'javascript:;',
+		            img: 'assets/imgs/list/cake.jpg',
+		            name: {
+		                cn: '芒果拿破仑',
+		                en: 'Fresh Mango Napoleon'
+		            },
+		            price: '189',
+		            pound: '1.5'
+		        }, {
+		            link: 'javascript:;',
+		            img: 'assets/imgs/list/cake.jpg',
+		            name: {
+		                cn: '芒果拿破仑',
+		                en: 'Fresh Mango Napoleon'
+		            },
+		            price: '189',
+		            pound: '1.5'
+		        }, {
+		            link: 'javascript:;',
+		            img: 'assets/imgs/list/cake.jpg',
+		            name: {
+		                cn: '芒果拿破仑',
+		                en: 'Fresh Mango Napoleon'
+		            },
+		            price: '189',
+		            pound: '1.5'
+		        }, {
+		            link: 'javascript:;',
+		            img: 'assets/imgs/list/cake.jpg',
+		            name: {
+		                cn: '芒果拿破仑',
+		                en: 'Fresh Mango Napoleon'
+		            },
+		            price: '189',
+		            pound: '1.5'
+		        }]
+			},
+			napoleon: {
+				list: [{
+		            link: 'javascript:;',
+		            img: 'assets/imgs/list/cake.jpg',
+		            name: {
+		                cn: '芒果拿破仑',
+		                en: 'Fresh Mango Napoleon'
+		            },
+		            price: '189',
+		            pound: '1.5'
+		        }, {
+		            link: 'javascript:;',
+		            img: 'assets/imgs/list/cake.jpg',
+		            name: {
+		                cn: '芒果拿破仑',
+		                en: 'Fresh Mango Napoleon'
+		            },
+		            price: '189',
+		            pound: '1.5'
+		        }, {
+		            link: 'javascript:;',
+		            img: 'assets/imgs/list/cake.jpg',
+		            name: {
+		                cn: '芒果拿破仑',
+		                en: 'Fresh Mango Napoleon'
+		            },
+		            price: '189',
+		            pound: '1.5'
+		        }, {
+		            link: 'javascript:;',
+		            img: 'assets/imgs/list/cake.jpg',
+		            name: {
+		                cn: '芒果拿破仑',
+		                en: 'Fresh Mango Napoleon'
+		            },
+		            price: '189',
+		            pound: '1.5'
+		        }]
+			},
+			mousse: {
+				list: [{
+		            link: 'javascript:;',
+		            img: 'assets/imgs/list/cake.jpg',
+		            name: {
+		                cn: '芒果拿破仑',
+		                en: 'Fresh Mango Napoleon'
+		            },
+		            price: '189',
+		            pound: '1.5'
+		        }, {
+		            link: 'javascript:;',
+		            img: 'assets/imgs/list/cake.jpg',
+		            name: {
+		                cn: '芒果拿破仑',
+		                en: 'Fresh Mango Napoleon'
+		            },
+		            price: '189',
+		            pound: '1.5'
+		        }, {
+		            link: 'javascript:;',
+		            img: 'assets/imgs/list/cake.jpg',
+		            name: {
+		                cn: '芒果拿破仑',
+		                en: 'Fresh Mango Napoleon'
+		            },
+		            price: '189',
+		            pound: '1.5'
+		        }, {
+		            link: 'javascript:;',
+		            img: 'assets/imgs/list/cake.jpg',
+		            name: {
+		                cn: '芒果拿破仑',
+		                en: 'Fresh Mango Napoleon'
+		            },
+		            price: '189',
+		            pound: '1.5'
+		        }]
+			},
+			milk: {
+				list: [{
+		            link: 'javascript:;',
+		            img: 'assets/imgs/list/cake.jpg',
+		            name: {
+		                cn: '芒果拿破仑',
+		                en: 'Fresh Mango Napoleon'
+		            },
+		            price: '189',
+		            pound: '1.5'
+		        }, {
+		            link: 'javascript:;',
+		            img: 'assets/imgs/list/cake.jpg',
+		            name: {
+		                cn: '芒果拿破仑',
+		                en: 'Fresh Mango Napoleon'
+		            },
+		            price: '189',
+		            pound: '1.5'
+		        }, {
+		            link: 'javascript:;',
+		            img: 'assets/imgs/list/cake.jpg',
+		            name: {
+		                cn: '芒果拿破仑',
+		                en: 'Fresh Mango Napoleon'
+		            },
+		            price: '189',
+		            pound: '1.5'
+		        }, {
+		            link: 'javascript:;',
+		            img: 'assets/imgs/list/cake.jpg',
+		            name: {
+		                cn: '芒果拿破仑',
+		                en: 'Fresh Mango Napoleon'
+		            },
+		            price: '189',
+		            pound: '1.5'
+		        }]
+			},
+			coffee: {
+				list: [{
+		            link: 'javascript:;',
+		            img: 'assets/imgs/list/cake.jpg',
+		            name: {
+		                cn: '芒果拿破仑',
+		                en: 'Fresh Mango Napoleon'
+		            },
+		            price: '189',
+		            pound: '1.5'
+		        }, {
+		            link: 'javascript:;',
+		            img: 'assets/imgs/list/cake.jpg',
+		            name: {
+		                cn: '芒果拿破仑',
+		                en: 'Fresh Mango Napoleon'
+		            },
+		            price: '189',
+		            pound: '1.5'
+		        }, {
+		            link: 'javascript:;',
+		            img: 'assets/imgs/list/cake.jpg',
+		            name: {
+		                cn: '芒果拿破仑',
+		                en: 'Fresh Mango Napoleon'
+		            },
+		            price: '189',
+		            pound: '1.5'
+		        }, {
+		            link: 'javascript:;',
+		            img: 'assets/imgs/list/cake.jpg',
+		            name: {
+		                cn: '芒果拿破仑',
+		                en: 'Fresh Mango Napoleon'
+		            },
+		            price: '189',
+		            pound: '1.5'
+		        }]
+			},
+			fruit: {
+				list: [{
+		            link: 'javascript:;',
+		            img: 'assets/imgs/list/cake.jpg',
+		            name: {
+		                cn: '芒果拿破仑',
+		                en: 'Fresh Mango Napoleon'
+		            },
+		            price: '189',
+		            pound: '1.5'
+		        }, {
+		            link: 'javascript:;',
+		            img: 'assets/imgs/list/cake.jpg',
+		            name: {
+		                cn: '芒果拿破仑',
+		                en: 'Fresh Mango Napoleon'
+		            },
+		            price: '189',
+		            pound: '1.5'
+		        }, {
+		            link: 'javascript:;',
+		            img: 'assets/imgs/list/cake.jpg',
+		            name: {
+		                cn: '芒果拿破仑',
+		                en: 'Fresh Mango Napoleon'
+		            },
+		            price: '189',
+		            pound: '1.5'
+		        }, {
+		            link: 'javascript:;',
+		            img: 'assets/imgs/list/cake.jpg',
+		            name: {
+		                cn: '芒果拿破仑',
+		                en: 'Fresh Mango Napoleon'
+		            },
+		            price: '189',
+		            pound: '1.5'
+		        }]
+			},
+			sugarfree: {
+				list: [{
+		            link: 'javascript:;',
+		            img: 'assets/imgs/list/cake.jpg',
+		            name: {
+		                cn: '芒果拿破仑',
+		                en: 'Fresh Mango Napoleon'
+		            },
+		            price: '189',
+		            pound: '1.5'
+		        }, {
+		            link: 'javascript:;',
+		            img: 'assets/imgs/list/cake.jpg',
+		            name: {
+		                cn: '芒果拿破仑',
+		                en: 'Fresh Mango Napoleon'
+		            },
+		            price: '189',
+		            pound: '1.5'
+		        }, {
+		            link: 'javascript:;',
+		            img: 'assets/imgs/list/cake.jpg',
+		            name: {
+		                cn: '芒果拿破仑',
+		                en: 'Fresh Mango Napoleon'
+		            },
+		            price: '189',
+		            pound: '1.5'
+		        }, {
+		            link: 'javascript:;',
+		            img: 'assets/imgs/list/cake.jpg',
+		            name: {
+		                cn: '芒果拿破仑',
+		                en: 'Fresh Mango Napoleon'
+		            },
+		            price: '189',
+		            pound: '1.5'
+		        }]
+			}
+		};
+
+		// 遍历所有的口味
+		$.each($aTasteItem, function(i, item) {
+			var $oCurrTaste = $(item),
+				tasteType = $oCurrTaste.attr('taste');
+
+			if(!!_data[tasteType]){
+				var _html = template('tplList', _data[tasteType]);
+				$oCurrTaste.append(_html);
+			}
+		});
+	}
 
 	function fnBindAll() {
 		var $oContainer = $('#secAll');
@@ -82,8 +433,25 @@
                 pound: '1.5'
             }]
         };
-        var _html = template('tplSecAll', _data);
+        var _html = template('tplList', _data);
         $oContainer.html(_html);
+	}
+
+	function fnChangeCategories() {
+		var $oListHeader = $('#listHeader'),
+			$oListContainer = $('#listContainer');
+
+		$oListHeader.on('click', '.c-item', function(){
+			var category = $(this).attr('category');
+
+			$(this).addClass('active').siblings().removeClass('active');
+
+			$oListContainer.find('.sec')
+				.filter('.sec[category=' + category + ']')
+				.addClass('active')
+				.siblings()
+				.removeClass('active');
+		});
 	}
 
 })(Zepto, window, document);
