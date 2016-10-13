b5df005715a94da334a3e205d1813d2d8937188c
armdong <armdong@163.com>
2016-10-13 11:28:35 +0800

    口味模块定位功能

--- src/assets/js/list.js
+++ src/assets/js/list.js
@@ -8,6 +8,9 @@
 
 	$(function(){
 
+		// 口味模块各种口味offset().top
+		window.tasteTops = {};
+
 		// 发现切换
 		fnChangeCategories();
 
@@ -16,21 +19,68 @@
 
 		// 绑定口味模块
 		fnBindTaste();
-
-		// 口味切换
-		fnChangeTaste();
+		
+		// 口味模块scroll事件
+		var lazyScroll = _.debounce(fnLocateTaste, 300);
+		$(window).on('scroll', lazyScroll);
 	});
 
+	/**
+	 * 滑动时定位口味类型
+	 * @return {[type]} [description]
+	 */
+	function fnLocateTaste() {
+		var scrollTop = $('body').scrollTop(),
+			currLocate = 'cheese';
+
+		if(scrollTop <= tasteTops.cheese - tasteTops.disT) {
+			currLocate = 'cheese';
+		} else if(scrollTop > tasteTops.chocolate - tasteTops.disT && scrollTop <= tasteTops.mousse - tasteTops.disT) {
+			currLocate = 'chocolate';
+		} else if(scrollTop > tasteTops.mousse - tasteTops.disT && scrollTop <= tasteTops.napoleon - tasteTops.disT) {
+			currLocate = 'mousse';
+		} else if(scrollTop > tasteTops.napoleon - tasteTops.disT && scrollTop <= tasteTops.fruit - tasteTops.disT) {
+			currLocate = 'napoleon';
+		} else if(scrollTop > tasteTops.fruit - tasteTops.disT && scrollTop <= tasteTops.milk - tasteTops.disT) {
+			currLocate = 'fruit';
+		} else if(scrollTop > tasteTops.milk - tasteTops.disT && scrollTop <= tasteTops.coffee - tasteTops.disT) {
+			currLocate = 'milk';
+		} else if(scrollTop > tasteTops.coffee - tasteTops.disT && scrollTop <= tasteTops.sugarfree - tasteTops.disT) {
+			currLocate = 'coffee';
+		} else if(scrollTop > tasteTops.sugarfree - tasteTops.disT) {
+			currLocate = 'sugarfree';
+		}
+		$('#secTaste').find('.nav-list').find('.nav-item')
+			.filter('div[taste=' + currLocate + ']')
+			.addClass('active')
+			.siblings()
+			.removeClass('active');
+	}
+
+	/**
+	 * 口味切换
+	 * @return {[type]} [description]
+	 */
 	function fnChangeTaste() {
-		var $oSecTaste = $('#secTaste');
+		var $oSecTaste = $('#secTaste'),
+			tl = new TimelineLite();
 
 		$oSecTaste.on('click', '.nav-item', function(){
 			$(this).addClass('active').siblings().removeClass('active');
 
-			
+			var currTaste = $(this).attr('taste');
+
+			tl.clear();
+			tl.to($('html,body'), 0.5, {
+				'scrollTop': tasteTops[currTaste] - tasteTops.disT + 5
+			});
 		});
 	}
 
+	/**
+	 * 绑定口味
+	 * @return {[type]} [description]
+	 */
 	function fnBindTaste() {
 		var $oSecTaste = $('#secTaste'),
 			$oTasteList = $oSecTaste.find('.taste-list'),
@@ -56,46 +106,10 @@
 		            },
 		            price: '189',
 		            pound: '1.5'
-		        }, {
-		            link: 'javascript:;',
-		            img: 'assets/imgs/list/cake.jpg',
-		            name: {
-		                cn: '芒果拿破仑',
-		                en: 'Fresh Mango Napoleon'
-		            },
-		            price: '189',
-		            pound: '1.5'
-		        }, {
-		            link: 'javascript:;',
-		            img: 'assets/imgs/list/cake.jpg',
-		            name: {
-		                cn: '芒果拿破仑',
-		                en: 'Fresh Mango Napoleon'
-		            },
-		            price: '189',
-		            pound: '1.5'
 		        }]
 			},
 			chocolate: {
 				list: [{
-		            link: 'javascript:;',
-		            img: 'assets/imgs/list/cake.jpg',
-		            name: {
-		                cn: '芒果拿破仑',
-		                en: 'Fresh Mango Napoleon'
-		            },
-		            price: '189',
-		            pound: '1.5'
-		        }, {
-		            link: 'javascript:;',
-		            img: 'assets/imgs/list/cake.jpg',
-		            name: {
-		                cn: '芒果拿破仑',
-		                en: 'Fresh Mango Napoleon'
-		            },
-		            price: '189',
-		            pound: '1.5'
-		        }, {
 		            link: 'javascript:;',
 		            img: 'assets/imgs/list/cake.jpg',
 		            name: {
@@ -143,37 +157,10 @@
 		            },
 		            price: '189',
 		            pound: '1.5'
-		        }, {
-		            link: 'javascript:;',
-		            img: 'assets/imgs/list/cake.jpg',
-		            name: {
-		                cn: '芒果拿破仑',
-		                en: 'Fresh Mango Napoleon'
-		            },
-		            price: '189',
-		            pound: '1.5'
 		        }]
 			},
 			mousse: {
 				list: [{
-		            link: 'javascript:;',
-		            img: 'assets/imgs/list/cake.jpg',
-		            name: {
-		                cn: '芒果拿破仑',
-		                en: 'Fresh Mango Napoleon'
-		            },
-		            price: '189',
-		            pound: '1.5'
-		        }, {
-		            link: 'javascript:;',
-		            img: 'assets/imgs/list/cake.jpg',
-		            name: {
-		                cn: '芒果拿破仑',
-		                en: 'Fresh Mango Napoleon'
-		            },
-		            price: '189',
-		            pound: '1.5'
-		        }, {
 		            link: 'javascript:;',
 		            img: 'assets/imgs/list/cake.jpg',
 		            name: {
@@ -203,55 +190,10 @@
 		            },
 		            price: '189',
 		            pound: '1.5'
-		        }, {
-		            link: 'javascript:;',
-		            img: 'assets/imgs/list/cake.jpg',
-		            name: {
-		                cn: '芒果拿破仑',
-		                en: 'Fresh Mango Napoleon'
-		            },
-		            price: '189',
-		            pound: '1.5'
-		        }, {
-		            link: 'javascript:;',
-		            img: 'assets/imgs/list/cake.jpg',
-		            name: {
-		                cn: '芒果拿破仑',
-		                en: 'Fresh Mango Napoleon'
-		            },
-		            price: '189',
-		            pound: '1.5'
-		        }, {
-		            link: 'javascript:;',
-		            img: 'assets/imgs/list/cake.jpg',
-		            name: {
-		                cn: '芒果拿破仑',
-		                en: 'Fresh Mango Napoleon'
-		            },
-		            price: '189',
-		            pound: '1.5'
 		        }]
 			},
 			coffee: {
 				list: [{
-		            link: 'javascript:;',
-		            img: 'assets/imgs/list/cake.jpg',
-		            name: {
-		                cn: '芒果拿破仑',
-		                en: 'Fresh Mango Napoleon'
-		            },
-		            price: '189',
-		            pound: '1.5'
-		        }, {
-		            link: 'javascript:;',
-		            img: 'assets/imgs/list/cake.jpg',
-		            name: {
-		                cn: '芒果拿破仑',
-		                en: 'Fresh Mango Napoleon'
-		            },
-		            price: '189',
-		            pound: '1.5'
-		        }, {
 		            link: 'javascript:;',
 		            img: 'assets/imgs/list/cake.jpg',
 		            name: {
@@ -338,15 +280,6 @@
 		            },
 		            price: '189',
 		            pound: '1.5'
-		        }, {
-		            link: 'javascript:;',
-		            img: 'assets/imgs/list/cake.jpg',
-		            name: {
-		                cn: '芒果拿破仑',
-		                en: 'Fresh Mango Napoleon'
-		            },
-		            price: '189',
-		            pound: '1.5'
 		        }]
 			}
 		};
@@ -361,8 +294,15 @@
 				$oCurrTaste.append(_html);
 			}
 		});
+
+		// 口味切换
+		fnChangeTaste();
 	}
 
+	/**
+	 * 绑定所有
+	 * @return {[type]} [description]
+	 */
 	function fnBindAll() {
 		var $oContainer = $('#secAll');
 
@@ -437,6 +377,10 @@
         $oContainer.html(_html);
 	}
 
+	/**
+	 * 发现类别切换
+	 * @return {[type]} [description]
+	 */
 	function fnChangeCategories() {
 		var $oListHeader = $('#listHeader'),
 			$oListContainer = $('#listContainer');
@@ -451,6 +395,18 @@
 				.addClass('active')
 				.siblings()
 				.removeClass('active');
+
+			// 如果是口味模块，计算每种口味的top值
+			if(category === 'taste') {
+				$('#secTaste').find('.taste-item').each(function(i, ele) {
+					var currTaste = $(this).attr('taste'),
+						currOffsetTop = Math.round($(this).offset().top);
+					tasteTops[currTaste] = currOffsetTop;
+					if (i === 0) {
+						tasteTops.disT = currOffsetTop;
+					}
+				});
+			}
 		});
 	}
 
