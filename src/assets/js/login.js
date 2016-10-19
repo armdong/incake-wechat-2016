e157be9e633de44a4b8148f02d7418fc76ee24db
armdong <armdong@163.com>
2016-10-19 20:31:46 +0800

    手机登录->手机号非空及合法性验证

--- src/assets/js/login.js
+++ src/assets/js/login.js
@@ -22,7 +22,51 @@
 
 	// 手机登录
 	function fnInitNormalLogin($oForm) {
+		var $oTxtMobile = $oForm.find('.txt-mobile'),
+			$oTxtVCode = $oForm.find('.txt-vcode'),
+			$oBtnVCode = $oForm.find('.btn-vcode'),
+			$oBtnLogin = $oForm.find('.btn-login');
 
+		// 获取短信验证码
+		$oBtnVCode.on('click', function(){
+
+			var txtMobile = $oTxtMobile.val();
+
+			// 验证手机号是否为空
+			if($.trim(txtMobile) === '') { 
+				var msg = '请输入手机号码';
+				txtMobileDialog(msg);
+	            return false;
+			}
+			
+			// 验证手机号是否是非法手机号
+			var reg = /^(?=\d{11}$)^1(?:3\d|4[57]|5[^4\D]|7[^249\D]|8\d)\d{8}$/;
+			if(!reg.test(txtMobile)) {
+				var msg = '请输入正确的手机号码';
+				txtMobileDialog(msg);
+				return false;
+			}
+
+			// TODO 获取验证码
+			
+
+		});
+
+		// 手机号码错误相关提示
+		function txtMobileDialog(msg) {
+			var dialog = $(document).dialog({
+                type: 'toast',
+                infoIcon: 'assets/plugins/dialog2/images/icon/fail.png',
+                infoText: msg,
+                autoClose: '1500',
+                onClosed: function() {
+                	// 清空输入框
+                	$oTxtMobile.val('');
+                	// 获取焦点
+                	$oTxtMobile.trigger('focus');
+                }
+            });
+		}
 	}
 
 	// 账号登录
