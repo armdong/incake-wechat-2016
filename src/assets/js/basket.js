c63b6d6ab602a8272f3b8be249d1821ee90eaa0a
armdong <armdong@163.com>
2016-10-28 11:21:16 +0800

    购物篮布局调整&消息轮播完善

--- src/assets/js/basket.js
+++ src/assets/js/basket.js
@@ -5,8 +5,39 @@
 		// 解决Zepto点透bug
 		FastClick.attach(document.body);
 
-		
+		// 购物篮公告模块
+        fnInitNotice();
 		
 	});
+
+	/**
+     * [fnInitNotice 购物篮公告模块]
+     * @return {[type]} [description]
+     */
+    function fnInitNotice() {
+        var $oNotice = $('#basketNotice'),
+            $oNoticeMsg = $oNotice.find('.notice-msg'),
+            $oNoticeClose = $oNotice.find('.notice-close'),
+            tl = new TimelineLite();
+
+        // 关闭公告
+        $oNoticeClose.on('tap', function() {
+            tl.clear();
+            tl.to($oNotice, 0.5, {
+                opacity: 0,
+                ease: Linear.easeOut,
+                useFrames: true,
+                onComplete: function() {
+                    $oNotice.remove();
+                }
+            });
+        });
+
+        // 公告轮播
+        var marqueeText = '订单满199元，可获得25元优惠券一张';
+        $oNoticeMsg.text(marqueeText);
+
+    	$oNoticeMsg.liMarquee();
+    }
 
 })(Zepto, window, document);
