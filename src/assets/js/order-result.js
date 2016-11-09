61cec2f13518d7dcbd017efc12c3e82fba26900d
armdong <armdong@163.com>
2016-11-09 20:15:10 +0800

    订单提交结果成功提示完成

--- src/assets/js/order-result.js
+++ src/assets/js/order-result.js
@@ -4,7 +4,40 @@
 
 		// 解决Zepto点透bug
 		FastClick.attach(document.body);
+
+		// 初始化订单支付结果
+		fnInitResult();
 		
 	});
+
+	/**
+	 * 初始化订单支付结果
+	 * @return {[type]} [description]
+	 */
+	function fnInitResult() {
+		var $oContainer = $('#pageContainer'),
+			$oSuccess = $oContainer.find('.success-container'),
+			$oFail = $oContainer.find('.fail-container');
+
+		// TODO 判断订单是否成功支付
+		var result = true;
+
+		if(result) { // 成功
+
+			// 订单支付方式：online 在线支付  offline: 货到付款/POS支付
+			var paytype = 'online',
+				$oMsg = $oSuccess.find('.msg');
+
+			if(paytype === 'online') {
+				$oMsg.find('.txt').children('span').html('订单已成功提交！');
+			} else {
+				$oMsg.find('.txt').children('span').html('支付成功！');
+			}
+
+			$oSuccess.show();
+		} else {
+			$oFail.show();
+		}
+	}
 
 })(Zepto, window, document);
