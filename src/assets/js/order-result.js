9c2733da754efe7432436addbd62b196e1af4bbc
armdong <armdong@163.com>
2016-11-10 10:37:19 +0800

    支付失败UI完成

--- src/assets/js/order-result.js
+++ src/assets/js/order-result.js
@@ -17,25 +17,33 @@
 	function fnInitResult() {
 		var $oContainer = $('#pageContainer'),
 			$oSuccess = $oContainer.find('.success-container'),
+			$oSucMsg = $oSuccess.find('.msg'),
+			paytype = 'online',
 			$oFail = $oContainer.find('.fail-container');
 
-		// TODO 判断订单是否成功支付
-		var result = true;
+		// TODO 判断订单是否成功支付 true:支付成功/提交成功  false:支付失败
+		var result = false;
 
-		if(result) { // 成功
+		if(result) { // 支付成功
 
-			// 订单支付方式：online 在线支付  offline: 货到付款/POS支付
-			var paytype = 'online',
-				$oMsg = $oSuccess.find('.msg');
+			// TODO 获取订单支付方式：
+			// online 在线支付  
+			// offline: 货到付款/POS支付
+			paytype = 'online';
 
 			if(paytype === 'online') {
-				$oMsg.find('.txt').children('span').html('订单已成功提交！');
+				// 在线支付
+				$oSucMsg.find('.txt').children('span').html('支付成功！');
 			} else {
-				$oMsg.find('.txt').children('span').html('支付成功！');
+				// 货到付款/货到POS支付
+				$oSucMsg.find('.txt').children('span').html('订单已成功提交！');
 			}
 
 			$oSuccess.show();
-		} else {
+		} else { // 支付失败
+
+
+
 			$oFail.show();
 		}
 	}
