45c5857c348580d4830f081fb5d3db1952aa6932
armdong <armdong@163.com>
2016-11-14 14:30:38 +0800

    新增地址布局完成

--- src/assets/js/order-address.js
+++ src/assets/js/order-address.js
@@ -6,15 +6,18 @@
 		FastClick.attach(document.body);
 
 		// 初始化地址列表
-		fnBindAddress();
+		fnInitAddress();
 	});
 
 	/**
 	 * 绑定收获地址
 	 * @return {[type]} [description]
 	 */
-	function fnBindAddress() {
-		var $oAddressList = $('#addressList');
+	function fnInitAddress() {
+		var $oAddressList = $('#addressList'),
+			$oNewAddress = $('#newAddress'),
+			$oAddressWrapper = $oAddressList.parent('.address-wrapper'),
+			tl = new TimelineLite();
 
 		var _data = {
 			hasAddress: true, // 地址列表是否有数据
@@ -102,6 +105,54 @@
 					area: '静安区',
 					street: '广中西路777弄99号506室'
 				}
+			}, {
+				isDefault: false,
+				isDisabled: false,
+				info: {
+					name: '张三',
+					mobile: '13322222222'
+				},
+				addr: {
+					city: '上海市',
+					area: '静安区',
+					street: '广中西路777弄99号506室'
+				}
+			}, {
+				isDefault: false,
+				isDisabled: false,
+				info: {
+					name: '张三',
+					mobile: '13322222222'
+				},
+				addr: {
+					city: '上海市',
+					area: '静安区',
+					street: '广中西路777弄99号506室'
+				}
+			}, {
+				isDefault: false,
+				isDisabled: false,
+				info: {
+					name: '张三',
+					mobile: '13322222222'
+				},
+				addr: {
+					city: '上海市',
+					area: '静安区',
+					street: '广中西路777弄99号506室'
+				}
+			}, {
+				isDefault: false,
+				isDisabled: false,
+				info: {
+					name: '张三',
+					mobile: '13322222222'
+				},
+				addr: {
+					city: '上海市',
+					area: '静安区',
+					street: '广中西路777弄99号506室'
+				}
 			}]
 		};
 		var _html = template('tplAddressList', _data);
@@ -113,6 +164,33 @@
         		.parent().siblings()
         		.children('.btn-radio')
         		.removeClass('selected');
+        });
+
+        // 新增收货地址
+        $oAddressList.on('tap', '.btn-new', function() {        	
+        	tl.clear();
+        	tl.to($oAddressWrapper, 0.5, {
+        		x: '-50%'
+        	});
+        });
+
+        // 编辑收货地址
+        $oAddressList.on('tap', '.btn-edit', function() {
+        	tl.clear();
+        	tl.to($oAddressWrapper, 0.5, {
+        		x: '-50%',
+        		onComplete: function() {
+        			$oAddressList.hide();
+        		}
+        	});
+        });
+
+        // 保存新增/编辑地址
+        $oNewAddress.on('tap', '.btn-save', function() {
+        	tl.clear();
+        	tl.to($oAddressWrapper, 0.5, {
+        		x: '0%'
+        	});
         });
 	}
 
