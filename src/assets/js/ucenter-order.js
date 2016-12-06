85f0558de6c91e79d926ce64c8478afa878e531f
armdong <armdong@163.com>
2016-12-06 13:20:05 +0800

    我的订单列表订单待支付订单支付倒计时完成

--- src/assets/js/ucenter-order.js
+++ src/assets/js/ucenter-order.js
@@ -38,7 +38,38 @@
                 }, {
                     className: 'btn-detail',
                     html: '查看详情'
-                }]
+                }],
+                pubTime: '2016/12/06 13:10:00' // 下单时间
+            }, {
+                time: '2016年12月12日 17:00-20:00 送达',
+                status: {
+                    className: 'unpay',
+                    text: '待付款'
+                },
+                orders: [{
+                    link: 'javascript:;',
+                    img: 'assets/imgs/ucenter/cake_01.jpg'
+                }, {
+                    link: 'javascript:;',
+                    img: 'assets/imgs/ucenter/cake_02.jpg'
+                }, {
+                    link: 'javascript:;',
+                    img: 'assets/imgs/ucenter/cake_01.jpg'
+                }, {
+                    link: 'javascript:;',
+                    img: 'assets/imgs/ucenter/cake_02.jpg'
+                }],
+                btns: [{
+                    className: 'btn-pay active',
+                    html: '去付款（<span class="countdown"><i class="minute">30</i>:<i class="second">00</i></span>）'
+                }, {
+                    className: 'btn-cacel',
+                    html: '取消订单'
+                }, {
+                    className: 'btn-detail',
+                    html: '查看详情'
+                }],
+                pubTime: '2016/12/06 12:50:00' // 下单时间
             }, {
                 time: '2016年12月12日 17:00-20:00 送达',
                 status: {
@@ -52,7 +83,8 @@
                 btns: [{
                     className: 'btn-detail',
                     html: '查看详情'
-                }]
+                }],
+                pubTime: '2016/12/06 11:00:00'
             }, {
                 time: '2016年12月12日 17:00-20:00 送达',
                 status: {
@@ -72,7 +104,8 @@
                 }, {
                     className: 'btn-contact',
                     html: '联系客服'
-                }]
+                }],
+                pubTime: '2016/12/06 11:00:00'
             }, {
                 time: '2016年12月12日 17:00-20:00 送达',
                 status: {
@@ -95,7 +128,32 @@
                 }, {
                     className: 'btn-detail',
                     html: '查看详情'
-                }]
+                }],
+                pubTime: '2016/12/06 11:00:00'
+            }, {
+                time: '2016年12月12日 17:00-20:00 送达',
+                status: {
+                    className: 'unconfirm',
+                    text: '待确认'
+                },
+                orders: [{
+                    link: 'javascript:;',
+                    img: 'assets/imgs/ucenter/cake_01.jpg'
+                }, {
+                    link: 'javascript:;',
+                    img: 'assets/imgs/ucenter/cake_02.jpg'
+                }, {
+                    link: 'javascript:;',
+                    img: 'assets/imgs/ucenter/cake_01.jpg'
+                }],
+                btns: [{
+                    className: 'btn-cancel',
+                    html: '取消订单'
+                }, {
+                    className: 'btn-detail',
+                    html: '查看详情'
+                }],
+                pubTime: '2016/12/06 11:00:00'
             }]
         };
         // 绑定订单信息
@@ -144,6 +202,9 @@
 
         // 初始化swiper
         handle4Swiper();
+
+        // 初始化倒计时
+        handle4Countdown();
     }
 
     /**
@@ -162,6 +223,75 @@
             observer: true,
             observeParents: true
         });
+    }
+
+    /**
+     * 初始化倒计时处理函数
+     * @return {[type]} [description]
+     */
+    function handle4Countdown() {
+
+    	// 拿到所有待支付订单倒计时DOM
+    	var $aCountDown = $('#orders').find('.countdown');
+
+    	// 遍历所有的倒计时DOM
+    	$.each($aCountDown, function(i, ele) {
+    		// 拿到下单时间
+    		var strTime = $(ele).closest('.item-footer').attr('data-pubtime'),
+    			beginTime = new Date(strTime);
+
+    		var endTime = beginTime.addMinutes(30),
+    			currShowTimeSeconds = 0;
+
+    		var $oMinute = $(ele).find('.minute'),
+    			$oSecond = $(ele).find('.second');
+
+    		currShowTimeSeconds = getCurrentShowTimeSeconds();
+
+    		ele.timer = setInterval(function() {
+    			render();
+    			update();
+    		}, 50);
+
+    		function update() {
+    			var nextShowTimeSeconds = getCurrentShowTimeSeconds();
+
+    			var nextHours = parseInt(nextShowTimeSeconds / 3600, 10),
+    				nextMinutes = parseInt((currShowTimeSeconds - nextHours * 3600) / 60, 10),
+    				nextSeconds = nextShowTimeSeconds % 60;
+
+    			var currHours = parseInt(currShowTimeSeconds / 3600, 10),
+    				currMinutes = parseInt((currShowTimeSeconds - currHours * 3600) / 60, 10),
+    				currSeconds = currShowTimeSeconds / 60;
+
+    			if(nextSeconds !== currSeconds) {
+    				currShowTimeSeconds = nextShowTimeSeconds;
+    			}
+    		}
+
+    		function render() {
+    			var hours = parseInt(currShowTimeSeconds / 3600, 10),
+    				minutes = parseInt((currShowTimeSeconds - hours * 3600) / 60, 10),
+    				seconds = currShowTimeSeconds % 60;
+
+    			if(hours === 0 && minutes === 0 && seconds === 0) {
+    				// TODO 处理取消订单动作
+    				// handle4CancelOrder();
+    				
+    				clearInterval(ele.timer);
+    			}
+
+    			$oMinute.html(minutes < 10 ? '0' + minutes : minutes);
+    			$oSecond.html(seconds < 10 ? '0' + seconds : seconds);
+    		}
+
+    		function getCurrentShowTimeSeconds() {
+    			var currTime = new Date();
+    			var ret = endTime.getTime() - currTime.getTime();
+    			ret = Math.round(ret / 1000);
+    			return ret >= 0 ? ret : 0;
+    		}
+    	});
     }
 
 })(Zepto, window, document);
